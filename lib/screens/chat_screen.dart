2eab72e2aff61d4998ae3f51bd04b0c8d7c408f8
Julian Bolaños <julian.bolanos.guerrero@gmail.com>
2023-11-25 14:38:10 -0600

    update now shows messages

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,22 +1,65 @@
+import 'package:chat_app_v1/widgets/messageIA.dart';
+import 'package:chat_app_v1/widgets/my_message.dart';
 import 'package:flutter/material.dart';
 
+// Vista principal de la app.
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    const imageChatGPT =
+        'https://cdn-1.webcatalog.io/catalog/chatgpt/chatgpt-icon-filled-256.png?v=1699261240952';
 
+    return Scaffold(
       appBar: AppBar(
         title: const Text('Chat GPT'),
         leading: const Row(
           children: [
             SizedBox(width: 16),
-            CircleAvatar(backgroundImage: NetworkImage('https://cdn-1.webcatalog.io/catalog/chatgpt/chatgpt-icon-filled-256.png?v=1699261240952')),
-            ],
-          ),
+            CircleAvatar(backgroundImage: NetworkImage(imageChatGPT)),
+          ],
         ),
-      
+      ),
+
+      // ' _ChatView() ' contiene toda la interfaz de la pantalla del chat.
+      body: const _ChatView(),
+    );
+  }
+}
+
+// '_chatView' contien toda la estrucutura del cuerpo de la app. En este widget
+// se define la organización en pantalla de los mensajes y la entrada de texto (input).
+class _ChatView extends StatelessWidget {
+  const _ChatView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 5.0),
+
+      // PENDIENTE.
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return (index % 2 == 0)
+                  ? const MyMessage()
+                  : const MessageIA();
+              },
+            ),
+          ),
+
+          // TODO: Actualizar para poner un input de texto. Sera la caja de mensajes.
+          Container(
+            width: 100,
+            height: 40,
+            color: Colors.blueGrey,
+          ),
+        ],
+      ),
     );
   }
 }
